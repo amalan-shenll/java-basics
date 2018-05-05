79ff145b248b963ab04edfe8732b4aab01663f40
Amalan Jenicious <amalan.f@shenll.com>
2018-05-05 15:59:39 +0530

    Initial setup

---
+++ src/javabasic/JavaBasic.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javabasic;
+
+/**
+ *
+ * @author shadmin
+ */
+public class JavaBasic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello There!");
+        System.out.println("Right click on Specific file \nand click Run File to run it");
+    }
+    
+}
