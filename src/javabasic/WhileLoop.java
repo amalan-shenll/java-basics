f9fe1c01bd0c725317b3ffd00d998aef948b8e59
Amalan Jenicious <amalan.f@shenll.com>
2018-05-05 17:05:08 +0530

    While loop example added

---
+++ src/javabasic/WhileLoop.java
@@ -0,0 +1,13 @@
+
+package javabasic;
+
+public class WhileLoop {
+    public static void main(String args[]){
+        int n=5;
+        int i=1;
+        while(i<=n){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
